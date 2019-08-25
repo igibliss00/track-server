0aa1dc4fb651c0be7d1cab480469a4f31312d5fd
J <igibliss00@gmail.com>
2019-08-24 23:02:47 -0400

    Added trackRoutes

--- src/models/User.js
+++ src/models/User.js
@@ -1,56 +1,58 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-})
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
 
 userSchema.pre('save', function(next) {
-    const user = this
-    if(!user.isModified('password')) {
-        return next()
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
     }
 
-    bcrypt.genSalt(10, (err, salt) => {
-        if(err) {
-            return next(err)
-        }
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if(err) {
-                return next(err)
-            }
-            user.password = hash
-        })
-    })
-})
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 userSchema.methods.comparePassword = function(candidatePassword) {
-    const user = this
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
-            if(err) {
-                return reject(err)
-            }
+  const user = this;
 
-            if(!isMatch) {
-                return reject(false)
-            }
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
 
-            resolve(true)
-        })
-    })
-}
+      if (!isMatch) {
+        return reject(false);
+      }
 
+      resolve(true);
+    });
+  });
+};
 
-
-mongoose.model('User', userSchema) 
+mongoose.model('User', userSchema);
 // don't export because mongoose only expects the model to be executed once
 // if imported to different components, model will be executed multiple times
 // there will be an error messages saying that User model has already been defined
